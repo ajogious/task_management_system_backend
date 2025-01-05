6f451a39df93177738d8c8f5b45db0a0a3a5470c
ajogious <ajogious@gmail.com>
2025-01-05 03:37:20 +0100

    updated

--- src/main/java/com/ajogious/task_management_backend/security/JWTUtil.java
+++ src/main/java/com/ajogious/task_management_backend/security/JWTUtil.java
@@ -12,12 +12,14 @@ import com.ajogious.task_management_backend.entities.User;
 import io.jsonwebtoken.*;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import lombok.RequiredArgsConstructor;
 
+@RequiredArgsConstructor
 @Service
 public class JWTUtil {
 
     @Value("${jwt.secret-key}")
-    private String secretKey;
+    private final String secretKey;
 
     // Token blacklisting
     private final Set<String> blacklistedTokens = new HashSet<>();
